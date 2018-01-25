3a6d7b927f4c7d320afceffa694866ccb4a7fac6
mirek.bartold <bartoldmir@gmail.com>
2018-01-25 23:26:34 +0100

    facebook authentication - fix bugs

--- src/main/java/com/mirek/oauth2app/Oauth2AppApplication.java
+++ src/main/java/com/mirek/oauth2app/Oauth2AppApplication.java
@@ -2,11 +2,10 @@ package com.mirek.oauth2app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
-@EnableOAuth2Sso
 public class Oauth2AppApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2AppApplication.class, args);
